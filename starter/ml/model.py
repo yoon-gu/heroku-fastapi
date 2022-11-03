237a5723159585cb2df8a3da52b5010059ea362b
Yoon-gu Hwang <yz0624@gmail.com>
2022-11-03 23:02:59 +0900

    add training code and evaluation code

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -18,7 +18,10 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +60,4 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
--- starter/train_model.py
+++ starter/train_model.py
@@ -5,7 +5,9 @@ from sklearn.model_selection import train_test_split
 # Add the necessary imports for the starter code.
 from ml import model
 from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference
 import pandas as pd
+
 
 # Add code to load in the data.
 data = pd.read_csv('../data/census.csv')
@@ -28,5 +30,15 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+print(precision, recall, fbeta)
