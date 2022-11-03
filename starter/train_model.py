b7e513ebe403265b70f04e596217fa6da966bfc5
Yoon-gu Hwang <yz0624@gmail.com>
2022-11-03 11:47:05 +0900

    add import code line

--- starter/train_model.py
+++ starter/train_model.py
@@ -3,8 +3,12 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from ml import model
+from ml.data import process_data
+import pandas as pd
 
 # Add code to load in the data.
+data = pd.read_csv('../data/census.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
