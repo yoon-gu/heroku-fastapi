4428bb3349208b86639a6b4797a6503b3c1d0ffd
Yoon-gu Hwang <yz0624@gmail.com>
2022-11-10 14:58:01 +0900

    done with testcase

--- test_main.py
+++ test_main.py
@@ -9,23 +9,46 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"greeting": "Hello World!"}
 
-def test_inference():
+def test_inference_small_salary():
     test_data = \
     {
-        "age" : 37,
-        "workclass" : "Private",
-        "fnlgt" : 280464,
-        "education" : "Some-college",
-        "education-num" : 10,
+        "age" : 39,
+        "workclass" : "State-gov",
+        "fnlgt" : 77516,
+        "education" : "Bachelors",
+        "education-num" : 13,
+        "marital-status" : "Never-married",
+        "occupation" : "Adm-clerical",
+        "relationship" : "Not-in-family",
+        "race" : "White",
+        "sex" : "Male",
+        "capital-gain" : 2174,
+        "capital-loss" : 0,
+        "hours-per-week" : 40,
+        "native-country" : "United-States"
+    }
+    response = client.post("/predict", json=test_data)
+    assert response.status_code == 200, \
+        f"Response code is not successful. {response.json()}"
+    assert response.json() == {"prediction":"<=50K"}
+
+def test_inference_large_salary():
+    test_data = \
+    {
+        "age" : 30,
+        "workclass" : "State-gov",
+        "fnlgt" : 141297,
+        "education" : "Bachelors",
+        "education-num" : 13,
         "marital-status" : "Married-civ-spouse",
-        "occupation" : "Exec-managerial",
+        "occupation" : "Prof-specialty",
         "relationship" : "Husband",
-        "race" : "Black",
+        "race" : "Asian-Pac-Islander",
         "sex" : "Male",
         "capital-gain" : 0,
         "capital-loss" : 0,
-        "hours-per-week" : 80,
-        "native-country" : "United-States"
+        "hours-per-week" : 40,
+        "native-country" : "India"
     }
     response = client.post("/predict", json=test_data)
     assert response.status_code == 200, \
